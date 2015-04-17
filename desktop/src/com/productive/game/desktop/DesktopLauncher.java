c38c5a769beab9fff71b0c17e204874b56bb0dc3
ojanoudi <ojanoudi@bu.edu>
2015-04-16 21:47:59 -0400

    This is the basic gdx framework we are going to begin with!

---
+++ desktop/src/com/productive/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.productive.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.productive.game.productive_game_class;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new productive_game_class(), config);
+	}
+}
---
+++ ios/src/com/productive/game/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.productive.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.productive.game.productive_game_class;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new productive_game_class(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
